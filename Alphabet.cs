3747884c126b012e6014b50d7f09f2cc55f7182b
Yety-wishdom <satanpower4225@gmail.com>
2024-04-11 21:30:00 +0500

    Добавил ЛР 1

---
+++ Alphabet.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+namespace Password_Ahalizator
+{
+    internal class Alphabet
+    {
+
+        public static int AlphabetLength(string password)
+        {
+            int alphabet_length = 0;
+
+            bool check_higher = false;
+            bool check_lower = false;
+            bool check_digits = false;
+            bool check_special = false;
+            char symbol;
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                symbol = Convert.ToChar(password.Substring(i, 1));
+
+                if (Char.IsDigit(symbol) == true && check_digits == false)
+                {
+                    check_digits = true;
+                }
+                if (Char.IsLower(symbol) == true && check_lower == false)
+                {
+                    check_lower = true;
+                }
+                if (Char.IsUpper(symbol) == true && check_higher == false)
+                {
+                    check_higher = true;
+                }
+                if (check_special == false && Char.IsUpper(symbol) == false && Char.IsLower(symbol) == false && Char.IsDigit(symbol) == false)
+                {
+                    check_special = true;
+                }
+            }
+            if (check_digits == true) { alphabet_length += 10; }
+            if (check_higher == true) { alphabet_length += 26; }
+            if (check_lower == true) { alphabet_length += 26; }
+            if (check_special == true) { alphabet_length += 33; }
+           
+            return alphabet_length;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_Ahalizator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Введите пароль, больше 5 символов:");
+                string password = "";
+                Console.WriteLine("Введите пароль");
+                password = Console.ReadLine();
+                
+                //Ввод параметров
+                Console.WriteLine("Введите скорость перебора паролей в секунду: ");
+                int s = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите количество неправильных попыток, при котырых идет пауза:");
+                int m = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите продолжительность паузы в сек.:");
+                int v = Convert.ToInt32(Console.ReadLine());
+
+                // Анализ и подсчет надежности пароля
+                int N = Alphabet.AlphabetLength(password);
+                double M = CombinationCount(password.Length, N);
+
+                double total_time = M / s;
+                int try_count = (int)M / m;
+                if ( M%m == 0)
+                {
+                    try_count -= 1;
+
+                }
+                double time_delay = try_count * v;
+                total_time += time_delay;
+
+
+                int totalSeconds = (int)total_time;
+
+                int years = totalSeconds / (365 * 24 * 60 * 60);
+                totalSeconds %= 365 * 24 * 60 * 60;
+
+                int months = totalSeconds / (30 * 24 * 60 * 60);
+                totalSeconds %= 30 * 24 * 60 * 60;
+
+                int days = totalSeconds / (24 * 60 * 60);
+                totalSeconds %= 24 * 60 * 60;
+
+                int hours = totalSeconds / (60 * 60);
+                totalSeconds %= 60 * 60;
+
+                int minutes = totalSeconds / 60;
+                totalSeconds %= 60;
+
+                int seconds = totalSeconds;
+
+
+            // Вывод результата
+            Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("---------------------------------------------------");
+                Console.WriteLine("Результаты анализа надежности пароля:");
+                Console.WriteLine("---------------------------------------------------");
+                
+                Console.WriteLine($"Мощность алфавита для подбора пароля:  {N}");
+                Console.WriteLine($"Мощность пространства паролей: {M}");
+                Console.WriteLine($"Время перебора всех возможных комбинаций: {years} лет {months} месяцев {days} дней {hours} часов {minutes} минут {seconds} секунд");
+                Console.Read();
+        }
+        public static double CombinationCount(int L, int N)
+        {
+            return Math.Pow(N, L);
+        }
+    }
+}
